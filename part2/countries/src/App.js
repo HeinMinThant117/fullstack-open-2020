39ee7b35a05fbddf28eab99d935c64d7ab57249c
Hemith <heinminthant1999@gmail.com>
2021-05-10 13:02:50 +0630

    Finished exercise 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const CountryDetailed = ({selectedCountry}) => {
+const CountryDetailed = ({ selectedCountry }) => {
+
+  const [weather, setWeather] = useState([])
+
+  useEffect(() => {
+    axios.get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${selectedCountry.capital}`)
+      .then(response => {
+        setWeather(response.data.current)
+      })  
+  
+  }, [])
+      console.log(weather);
+
   return (
     <div>
       <h1>
@@ -11,13 +23,22 @@ const CountryDetailed = ({selectedCountry}) => {
       <p>capital {selectedCountry.capital}</p>
       <p>population {selectedCountry.population}</p>
 
-      <h3>
-        languages
-    </h3>
+      <h3>languages</h3>
       <ul>
         {selectedCountry.languages.map(language => <li key={language.name}>{language.name}</li>)}
       </ul>
-      <img width="180px" src={selectedCountry.flag} />
+      <img width="180px" src={selectedCountry.flag} alt='country flag '/>
+
+      <h3>weather in {selectedCountry.capital}</h3>
+      <p>temperature : {weather.temperature} Celcius</p>
+      
+      <img src={weather.weather_icons} alt="forecast" />
+      
+      <p>wind : {weather.wind_speed} mph direction {weather.wind_dir}</p>
+
+
+
+
     </div>
   )
 }
@@ -36,7 +57,7 @@ const Country = (props) => {
 
   const handleClick = () => setShowCountry(true)
 
-  if(showCountry) {
+  if (showCountry) {
     return (
       <CountryDetailed selectedCountry={props.country} />
     )
@@ -44,7 +65,7 @@ const Country = (props) => {
 
   return (
     <div>
-      {props.country.name} 
+      {props.country.name}
       <button onClick={handleClick}>Show</button>
     </div>
   )
@@ -54,7 +75,7 @@ const Countries = (props) => {
 
   const filteredCountries = props.countries.filter(country => country.name.toLowerCase().includes(props.newCountry.toLowerCase()))
 
-  
+
 
   if (filteredCountries.length > 10) {
     return (
@@ -67,22 +88,21 @@ const Countries = (props) => {
   if (filteredCountries.length === 1) {
     const selectedCountry = filteredCountries[0]
     return (
-      <CountryDetailed selectedCountry={selectedCountry}/>
+      <CountryDetailed selectedCountry={selectedCountry} />
     )
-    
+
 
   }
 
-  console.log(filteredCountries)
 
   return (
     <div>
-      {filteredCountries.map(country => 
-          <Country key={country.name} country={country}></Country>
+      {filteredCountries.map(country =>
+        <Country key={country.name} country={country}></Country>
 
         // <p key={country.name}>{country.name}</p>
         // <button>Show</button>
-      
+
       )}
 
     </div>
@@ -93,6 +113,7 @@ const Countries = (props) => {
 }
 
 const App = () => {
+
 
   const [countries, setCountries] = useState([])
   const [newCountry, setNewCountry] = useState('')
@@ -111,8 +132,8 @@ const App = () => {
 
   return (
     <div>
-      <CountrySearch newCountry={newCountry} handleNewCountryChange={handleNewCountryChange}  />
-      <Countries countries={countries} newCountry={newCountry}/>
+      <CountrySearch newCountry={newCountry} handleNewCountryChange={handleNewCountryChange} />
+      <Countries countries={countries} newCountry={newCountry} />
     </div>
   );
 }
